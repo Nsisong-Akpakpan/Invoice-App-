086afb505924598cb56ec3834aad62208db8e960
Nsisong-Akpakpan <93917408+Nsisong-Akpakpan@users.noreply.github.com>
2022-12-12 14:47:51 +0100

    my commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MainPage from "./MainPage";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { BrowswerRouter as Router,
+//         Routes,
+//         Route } from "react-router-dom";
+// import { Container, Box } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    default: {
+      main: "#969696",
+    },
+    primary: {
+      main: "#EE9209",
+    },
+    secondary: {
+      main: "#0D0AB8",
+    },
+    inherit: {
+      main: "#969696",
+    },
+  },
+
+  overrides: {
+    MuiFormControl: {
+      root: {
+        height: '0px',
+      },
+    },
+    MuiInputBase: {
+      root: {
+        height: '0px',
+      },
+    },
+  },
+  
+  // palette: {
+  //   primary: {
+  //     main: "#0D0AB8",
+  //   },
+  //   secondary: {
+  //     main: "#414744",
+  //   },
+  // },
+  typography: {
+    fontFamily: "Inter",
+  },
+  shape: {
+    borderRadius: 0,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+         <MainPage/>
+      </ThemeProvider>
     </div>
   );
 }
-
 export default App;
+
